d8aa93ee847fc0673006d3e2e2c91bb8830cca37
Frolova <olka_frol@mail.ru>
2020-11-19 10:19:02 -0500

    add double,float function

--- modules/task_2/frolova_o_bcast/bcast.h
+++ modules/task_2/frolova_o_bcast/bcast.h
@@ -6,11 +6,15 @@
 #include <string>
 
 std::vector<int> getRandomArray(int size);
+std::vector<double> getRandomDoubleArray(int size);
+std::vector<float> getRandomFloatArray(int size);
 int checkSum(std::vector<int> array, int size);
+double checkDoubleSum(std::vector<double> array, int size);
+float checkFloatSum(std::vector<float> array, int size);
 void my_Bcast(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm);
-int forProc(int cur, int root, int procNum);
-int fromProc(int cur, int root, int procNum);
 void getSum(std::vector<int> array, int size, int *sum, int root);
+void getDoubleSum(std::vector<double> array, int size, double *sum, int root);
+void getFloatSum(std::vector<float> array, int size, float *sum, int root);
 void difOfTime(int count, int root);
 
 #endif  // MODULES_TASK_2_FROLOVA_O_BCAST_BCAST_H_
